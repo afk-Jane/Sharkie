0bb342f2f4c571cf2de26c8f0147c2151b1d40ef
afk-Jane <j.domsgen@protonmail.com>
2025-10-23 13:20:21 +0200

    stash

---
+++ js/classes/music-manager.class.js
@@ -0,0 +1,35 @@
+class MusicManager {
+    constructor(src, loop = true) {
+        this.audio = new Audio(src);
+        this.audio.loop = loop;
+
+        const settings = new SettingsManager();
+        this.audio.volume = settings.get("volume") ?? 0.5;
+        this.audio.muted = settings.get("mute") ?? false;
+    }
+
+    play() {
+        this.audio.play();
+    }
+
+    pause() {
+        this.audio.pause();
+    }
+
+    stop() {
+        this.audio.pause();
+        this.audio.currentTime = 0;
+    }
+
+    setVolume(volume) {
+        this.audio.volume = volume;
+    }
+
+    mute() {
+        this.audio.muted = true;
+    }
+
+    unmute() {
+        this.audio.muted = false;
+    }
+}
